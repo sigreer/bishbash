34307bb07086aedaee1fa956b908600d456d5e7e
Simon Greer <simon.greer@tilttech.co.uk>
2023-01-03 06:40:10 +0000

    cleanup

--- ubuntudockercompose.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## Updated 21/08/22
-
-COMPOSE_VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
-COMPOSE_DESTINATION=/usr/local/bin/docker-compose
-curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $COMPOSE_DESTINATION
-chmod +x $COMPOSE_DESTINATION
