104369b7fb14ef381492b0bdaea4328a16f1e6d4
Simon Greer <simon.greer@tilttech.co.uk>
2023-01-04 23:31:56 +0000

    quickpush

--- dockerubuntu.sh
+++ dockerubuntu.sh
@@ -12,5 +12,5 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 docker run hello world && echo "Installed successfully"
