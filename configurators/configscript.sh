82bc05e445a6b1530c5f1a6b862d6e0783bc9d89
sigreer <42386868+sigreer@users.noreply.github.com>
2023-01-24 13:41:11 +0000

    sorted scripts

--- configurators/configscript.sh
+++ configurators/configscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-[[ -e /etc/os-release ]] && . /etc/os-release && osname=$(echo "$NAME") && osversion=$VERSION_ID # freedesktop.org and systemd
-[[ -e /etc/lsb-release ]] && . /etc/lsb-release && osname=$DISTRIB_ID && osversion=$DISTRIB_RELEASE # For some versions of Debian/Ubuntu without lsb_release command
+[[ -e /etc/os-release ]] && . /etc/os-release && osname="$NAME" && osversion=$VERSION_ID # freedesktop.org and systemd
+[[ -e /etc/lsb-release ]] && . /etc/lsb-release && osname="$DISTRIB_ID" && osversion="$DISTRIB_RELEASE" # For some versions of Debian/Ubuntu without lsb_release command
 #[[ -e /etc/debian_version ]] && osversion=`. /etc/debian_verion`; osname="Debian" # Older Debian/Ubuntu/etc.
 [[ -e /etc/SuSe-release ]] && osname="SuSe" ## Incomplete
 [[ -e /etc/redhat-release ]] && osname="Redhat" ## Incomplete
@@ -13,21 +13,21 @@ instajdebbash () {
     grep -qxF 'autojump' ~/.bashrc || echo '. /usr/share/autojump/autojump.sh' >> ~/.bashrc
 
 # shellcheck source=$HOME/.bashrc
-    source $HOME/.bashrc
+    . "$HOME"/.bashrc
 }
 
 installautojump () {
     if [[ $osname =~ "Debian" || $osname =~ "buntu" && $currentshell == "bash" ]]; then
         instajdebbash
     else
-        cd ~/
+        cd "$HOME" || exit
         git clone git://github.com/wting/autojump.git
-        cd autojump
+        cd autojump || exit
         ./install.py
-        [[ -e "/usr/share/autojump" ]] && cd ~/ 
+        [[ -e "/usr/share/autojump" ]] && cd ~/ || exit 
         rm -R ./autojump
     fi
-    grep -qxF "autojump" ~/."$currentshell"rc || echo "Autojump isn't referenced in ~/."$currentshell"rc. Pleae check installation" && echo "Autojump installed"
+    grep -qxF "autojump" ~/."$currentshell"rc || echo "Autojump isn't referenced in ~/.""$currentshell""rc. Pleae check installation" && echo "Autojump installed"
     }
 
 installaj () { aji=1; while [ $aji -le 3 ]; do installautojump aji=$(( aji++ )); done }
@@ -42,14 +42,27 @@ checkNala () {
 if [[ -e "/usr/share/docs/nala" ]]; then
     nalainstalled=1
 fi
+shellrcnala=$(cat "$shellconfigfile" | grep nala)
+if [[ -n $shellrcnala ]]; then
+    nalainshell=1
+fi
 }
 
 installnala () {
     checkNala
-	if [[ $ostype == "Debian" || $ostype =~ "buntu" ]] && [[ $nalainstalled != 1 ]]; then
+    if [[ $nalainstalled == 1 && $nalainshell == 1 ]]; then
+        return
+    fi
+	if [[ ! $ostype =~ .*ebian || ! $ostype =~ .*untu ]]; then
+        return
+    fi
+    if [[ $ostype == "Debian" || $ostype =~ "buntu" ]] && [[ $nalainstalled != 1 ]]; then
 	echo "deb http://deb.volian.org/volian/ scar main" | tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list; wget -qO - https://deb.volian.org/volian/scar.key | tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
 	apt update && apt install nala -y
-cat <<EOI >> ~/.${currentshell}rc
+    if [[ $nalainshell == 1 ]]; then
+        return
+    fi
+cat <<EOI >> "$shellconfigfile"
 apt() { 
   command nala "\$@"
 }
@@ -69,7 +82,7 @@ installdops () {
 	if [[ ! -f /usr/bin/dops ]]; then
 	if [[ $ostype == "Debian" || $ostype =~ "buntu" ]]; then
 		wget https://github.com/Mikescher/better-docker-ps/releases/download/v1.6/dops_linux-amd64
-        mv dops_linux-amd4 /usr/bin/dops
+        mv ./dops_linux-amd4 /usr/bin/dops
 		chmod +x /usr/bin/dops
 	fi
 	if [[ $ostype == "Arch" ]]; then
@@ -81,13 +94,13 @@ installdops () {
 [[ -e /usr/bin/dops ]] && dopsinst="Yes"
 [[ -e /usr/share/docs/nala ]] && nalainst="Yes"
 [[ -z $XDG_CURRENT_DESKTOP ]] && systemtype="server" || systemtype="desktop" ## OK
-[[ $systemtype == "desktop" ]] && sessiontype=$(echo $GDMSESSION$XDG_SESSION_TYPE) && desktopenv=$(echo $XDG_CURRENT_DESKTOP) ## OK
+[[ $systemtype == "desktop" ]] && sessiontype=$(echo "$GDMSESSION"$XDG_SESSION_TYPE) && desktopenv=$(echo "$XDG_CURRENT_DESKTOP") ## OK
 [[ -e "/usr/bin/autojump" ]] && autojumppath="/usr/bin/autojump" && autojumpinst="Yes" || echo "/usr/bin/autojump doesn't exist" ## OK
-[[ $SHELL =~ "zsh" ]] && currentshell="zsh" && shellconfigfile="~/.zshrc"
-[[ $SHELL =~ "bash" ]] && currentshell="bash" && shellconfigfile="~/.bashrc"
+[[ $SHELL =~ "zsh" ]] && currentshell="zsh" && shellconfigfile="$HOME"/.zshrc
+[[ $SHELL =~ "bash" ]] && currentshell="bash" && shellconfigfile="$HOME"/.bashrc
 [[ -e /usr/share/autojump ]] || installaj && echo "Autojump not found, installing..."
-filecontainsaj=$(cd ~/ && grep ".*autojump.*" .${currentshell}rc) 
-[[ -z $filecontentsaj ]] && autojumploaded="No" && addajtoshell || autojumploaded="Yes" 
+filecontainsaj=$(cd ~/ && grep ".*autojump.*" "$shellconfigfile") 
+[[ -z $filecontainsaj ]] && autojumploaded="No" && addajtoshell || autojumploaded="Yes" 
 [[ $osname =~ "ebian" || $osname =~ "buntu" ]] && ostype="Debian"
 [[ $osname =~ "anjaro" || $osname =~ "rch" ]] && ostype="Arch"
 shownala=$( [[ $ostype =~ "ebian" ]] && echo "Nala:" "$nalainst" )
@@ -100,7 +113,8 @@ System Type: $systemtype
 Desktop Environment: $desktopenv
 Session: $sessiontype
 Shell: $SHELL$shownala
-Autojump: $autojumpinst
+Autojump Installed: $autojumpinst
+Autojump Loaded: $autojumploaded
 Dops: $dopsinst
 EndOfMessage
 
@@ -119,7 +133,7 @@ alias comp2='docker-compose up'
 alias recomp='docker compose down && docker-compose up'
 alias comp='docker compose up'
 alias comped='nano docker-compose.yml'
-alias compbk='cp docker-compose.yml docker-compose.backup`echo \$\("date"\)`.yml'
+alias compbk='cp docker-compose.yml docker-compose.backup$(echo \$\("date"\)).yml'
 EOB
 )
 fi
@@ -130,22 +144,22 @@ EOB
 )
 
 function add_custom_aliases () {
-    [[ -e ~/.${currentshell}rc ]] && echo "found ~/."$currentshell"rc"
+    [[ -e ~/.${currentshell}rc ]] && echo "found ~/.""$currentshell""rc"
     if [[ -e ~/.${currentshell}_custom_aliases ]]; then
-        echo "Found ~/."$currentshell"_custom_aliases. Deleting..."
+        echo "Found ~/.""$currentshell""_custom_aliases. Deleting..."
         rm ~/."$currentshell"_custom_aliases
-        echo "Deleted ~/."$currentshell"_custom_aliases. Recreating..."
+        echo "Deleted ~/.""$currentshell""_custom_aliases. Recreating..."
     fi
 cat > ~/."$currentshell"_custom_aliases <<ALIASES 
 ${aliases_general}
 ${aliases_docker}
 ${aliases_git}
 ALIASES
-    [[ -e ~/."$currentshell"_custom_aliases ]] && echo "created ~/."$currentshell"_custom_aliases"
-    echo "Checking for reference in ~/."$currentshell"rc for ~/."$currentshell"_custom_aliases"
-    grep -qxF "source ~/.${currentshell}_custom_aliases" ~/."$currentshell"rc || echo "source ~/."$currentshell"_custom_aliases" >> ~/."$currentshell"rc && echo "Already present, continuing..."
-    echo "Sourcing~/."$currentshell"_custom_aliases"
-    source ~/."$currentshell"_custom_aliases
+    [[ -e ~/."$currentshell"_custom_aliases ]] && echo "created ~/.""$currentshell""_custom_aliases"
+    echo "Checking for reference in ~/.""$currentshell""rc for ~/.""$currentshell""_custom_aliases"
+    grep -qxF "source ~/.${currentshell}_custom_aliases" ~/."$currentshell"rc || echo "source ~/.""$currentshell""_custom_aliases" >> ~/."$currentshell"rc && echo "Already present, continuing..."
+    echo "Sourcing~/.""$currentshell""_custom_aliases"
+    . "$HOME"/."$currentshell"_custom_aliases
     echo "Done setting up aliases."
 }
 
