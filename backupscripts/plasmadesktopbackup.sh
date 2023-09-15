4b68402b8a171678c23b350fb99ab4a032103498
sigreer <simon.greer@tilttech.co.uk>
2023-09-15 21:52:17 +0100

    tweak

--- backupscripts/plasmadesktopbackup.sh
+++ backupscripts/plasmadesktopbackup.sh
@@ -5,7 +5,7 @@ cp -R ~/.config/lattedock ~/tempbackup/.config/
 cp ~/.config/{plasmawindowedrc,plasmarc,plasma-org.kde.plasma.desktop-appletsrc,plasmanotifyrc,plasma-localerc,plasma-emojierrc} ~/tempbackup/.config/cp -R ~/.config/plasma-workspace ~/tempbackup/.config/
 NAME=desktop_configuration_backup$(date '+%Y-%m-%d')
 tar -cvzf "$NAME.tar.gz" ~/tempbackup/*
-echo "$NAME.tar.gz copied to $pwd"
+echo "$NAME.tar.gz copied to $(pwd)"
 rm -R ~/tempbackup
 echo "temporary files removed"
 echo "complete"
